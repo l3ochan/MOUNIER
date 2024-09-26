badf13cf86d4d5eb72d9163e3c7190578d391d06
Leochan <leonard@nekocorp.fr>
2024-09-26 08:15:25 +0200

    gioerjgoehorhoierhb

---
+++ simpleClientSocket.py
@@ -0,0 +1,10 @@
+import socket
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+port = 62154
+server_ip = "nekoserv.diplodocus-anaconda.ts.net"
+client.connect(server_ip, port)
+print(f"connecting to {server_ip}:{port}")
+client.sendall(b"hello serveur\n")
+client.close()
+
+
